0a65c14fddc1f450da733837c78d2b66d44856d5
Robie Mendiola <robie_mendiola@yahoo.com>
2024-03-26 22:03:54 +0800

    Responsive design for mobile

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -4,7 +4,7 @@ import SidebarNavs from './SidebarNavs';
 import SidebarFooter from './SidebarFooter';
 const Sidebar = () => {
   return (
-    <div className="border-r-1 relative mx-2 mb-4 h-[95%] min-w-[20%] overflow-x-hidden rounded-md border bg-white p-5">
+    <div className="border-r-1 relative mx-2 mb-4 h-[95%] min-h-[340px] min-w-[20%] overflow-x-hidden rounded-md border bg-white p-5">
       <SidebarNavs />
       <SidebarFooter />
     </div>
--- src/components/UserTabs.jsx
+++ src/components/UserTabs.jsx
@@ -3,7 +3,7 @@ import { tabOptions } from '../utils/utils';
 
 const UserTabs = ({ selectedTab, setSelected }) => {
   return (
-    <div className="h-18 w-autoflex flex flex-col rounded border bg-slate-50 p-2 font-medium text-zinc-500 md:flex-row">
+    <div className=" flex min-h-[65px] flex-col overflow-x-auto rounded border bg-slate-50 p-2 font-medium text-zinc-500 md:flex-row">
       {tabOptions?.map((item, index) => (
         <button
           key={index}
