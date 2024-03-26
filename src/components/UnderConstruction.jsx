8f9f0f187fe920534f1e383760af0faf6a34efee
Robie Mendiola <robie_mendiola@yahoo.com>
2024-03-26 19:58:59 +0800

    Minor fixes

--- src/components/UnderConstruction.jsx
+++ src/components/UnderConstruction.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const UnderConstruction = () => {
   return (
-    <div className="w-3/4 pt-6">
-      <div className="border-r-1 h-screen rounded-md border bg-white p-5 ">
+    <div className=" h-5/6 w-3/4 px-5 pb-2 pt-6 ">
+      <div className="border-r-1 h-full overflow-auto rounded-md border bg-white p-5">
         This Page is currently under development.
       </div>
     </div>
